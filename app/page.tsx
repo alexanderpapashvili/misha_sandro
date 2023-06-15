afac70cf25d882aa82ebdb3ba16e3aa1e50a60a4
alexanderpapashvili <sandropapashvili@gmail.com>
2023-06-15 19:06:51 +0400

    button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.css'
 import Header from './components/header/Header'
+import Button from './components/button/Button'
 
 export default function Home() {
   return (
     <main className={styles.main}>
      <Header />
+     <Button /> 
     </main>
   )
 }
