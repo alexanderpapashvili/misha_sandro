09866176f083d46de5a01e591893bd6e614df11d
alexanderpapashvili <sandropapashvili@gmail.com>
2023-06-16 12:35:23 +0400

    add header in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css'
 import Button from './components/button/Button'
+import Header from './components/header/Header'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Header />
      <Button /> 
     </main>
   )
