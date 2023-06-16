cc98c4319f95f5814ae36efbadb44214b3a21110
Misha <misha@novatori.ge>
2023-06-16 12:38:14 +0400

    feat: Created reusable Icon component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import Button from './components/button/Button'
 import Header from './components/header/Header'
+import Icon from './components/Icon/Icon'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
-      <Button /> 
+      <Icon />
+     <Button /> 
     </main>
   )
 }
