371858dd77baa978f13c7a5343dbda9f4e3d63a7
Misha <misha@novatori.ge>
2023-06-16 12:33:41 +0400

    remove header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import styles from './page.module.css'
 import Button from './components/button/Button'
-import Header from './components/header/Header'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header />
      <Button /> 
     </main>
   )
